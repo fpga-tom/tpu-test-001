f08777bd1047cec63c90e7306f23e97ebc0ba56b
tomas <tomaskrajco@gmail.com>
2018-08-27 13:19:48 +0200

    Dockerfile

--- mnist/tpu_test.py
+++ mnist/tpu_test.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from absl import app as absl_app
+from absl import flags
 import tensorflow as tf
 
 import mnist
