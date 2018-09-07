91acb5e0e4954b3a194bbdf8921d1a054303d607
tomas <tomaskrajco@gmail.com>
2018-09-07 19:25:50 +0200

    cube

--- deepxor/cube.py
+++ deepxor/cube.py
@@ -60,7 +60,7 @@ def predict_input_fn(params):
             (tf.float32, tf.float32, tf.float32, tf.float32),
             (tf.TensorShape([len_solved]), tf.TensorShape([len_solved]),  tf.TensorShape([]), tf.TensorShape([])))
     ds = ds.map(lambda s, c, r, i: {'state': s, 'parent': c, 'reward': r, 'distance': i})
-    return ds.batch(FLAGS.batch_size)
+    return dsapply(tf.contrib.data.batch_and_drop_remainder(FLAGS.batch_size)).make_one_shot_iterator().get_next()
 
 train_samples = []
 
@@ -95,7 +95,7 @@ def adi(est, cpu_est):
                 train_samples.append((buf[0]['parent'], y_p, y_v, buf[0]['distance']))
                 buf = []
 
-#        est.train(train_input_fn, max_steps=next_checkpoint)
+        est.train(train_input_fn, max_steps=next_checkpoint)
         current_step = next_checkpoint
 
 
