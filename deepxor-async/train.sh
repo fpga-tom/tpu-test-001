41d2224d6ee655b9afc916a16948747dabe664f8
Your Name <tomaskrajco@instance-1.c.tpu-test-001.internal>
2018-09-15 19:41:06 +0000

    train.sh

--- deepxor-async/train.sh
+++ deepxor-async/train.sh
@@ -1,27 +1,23 @@
 # On ps0.example.com:
+WORKER_HOSTS=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227,localhost:2228,localhost:2229,localhost:2230,localhost:2231,localhost:2232,localhost:2233,localhost:2234
+WORKERS="0 1 2 3 4 5 6 7 8 9 10 11"
 python3 cube.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226 \
+     --ps_hosts=localhost:2221,localhost:2222 \
+     --worker_hosts=$WORKER_HOSTS \
      --job_name=ps --task_index=0 &
+echo "kill $!" > stop.sh
+python3 cube.py \
+     --ps_hosts=localhost:2221,localhost:2222 \
+     --worker_hosts=$WORKER_HOSTS \
+     --job_name=ps --task_index=1 &
+echo "kill $!" >> stop.sh
 # On worker0.example.com:
+for idx in $WORKERS; do
 python3 cube.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226 \
-     --job_name=worker --task_index=0 \
+     --ps_hosts=localhost:2221,localhost:2222 \
+     --worker_hosts=$WORKER_HOSTS \
+     --job_name=worker --task_index=$idx \
      --rolls=64 --rolls_len=32 &
-# On worker1.example.com:
-python3 cube.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226 \
-     --job_name=worker --task_index=1 \
-     --rolls=64 --rolls_len=32 &
-python3 cube.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226 \
-     --job_name=worker --task_index=2 \
-     --rolls=64 --rolls_len=32 &
-python3 cube.py \
-     --ps_hosts=localhost:2222 \
-     --worker_hosts=localhost:2223,localhost:2224,localhost:2225,localhost:2226 \
-     --job_name=worker --task_index=3 \
-     --rolls=64 --rolls_len=32 &
+echo "kill $!" >> stop.sh
+done
+chmod u+x stop.sh
