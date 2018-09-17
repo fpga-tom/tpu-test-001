cbb3e33bb7d9a0ad7a120a45cc40636ea6cec0b3
tomas <tomaskrajco@instance-1.c.tpu-test-001.internal>
2018-09-17 20:15:47 +0000

    mpi

--- deepxor-async/run-mpi.sh
+++ deepxor-async/run-mpi.sh
@@ -1,2 +1,2 @@
-MODEL_DIR=/home/tomas/Documents/deepxor-async-gd-9
-/usr/lib64/openmpi/bin/mpirun -np 8 python3 cube.py  --rolls=128 --rolls_len=64 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 --train_steps_per_eval=200
+MODEL_DIR=gs://tomaskrajco-tpu-test-001-bucket/deepxor-async-gd-9
+mpirun -np 8 python3 cube.py  --rolls=128 --rolls_len=64 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 --train_steps_per_eval=200
