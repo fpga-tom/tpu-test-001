73e79b8ce75edb89480eeaa965bebad84e99593f
tomas <tomaskrajco@gmail.com>
2018-09-17 20:16:50 +0200

    train

--- deepxor-async/train.sh
+++ deepxor-async/train.sh
@@ -1,7 +1,7 @@
 # On ps0.example.com:
 #WORKER_HOSTS=localhost:2223,localhost:2224,localhost:2225,localhost:2226,localhost:2227,localhost:2228,localhost:2229,localhost:2230
 WORKERS=`seq 0 7`
-WORKER_HOSTS=''
+WORKER_HOSTS='192.168.2.103:2250,'
 for idx in $WORKERS; do
 	if [ "$idx" -eq "0" ]; then
 		WORKER_HOSTS=localhost:$((2223+$idx))
@@ -10,11 +10,11 @@ for idx in $WORKERS; do
 	fi
 done
 
-MODEL_DIR=gs://tomaskrajco-tpu-test-001-bucket/deepxor-async-gd-6
+MODEL_DIR=/home/tomas/Documents/deepxor-async-gd-7
 CUDA_VISIBLE_DEVICES='' python3 cube.py \
      --ps_hosts=localhost:2222 \
      --worker_hosts=$WORKER_HOSTS \
-     --job_name=ps --task_index=0 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 &
+     --job_name=ps --task_index=0 --model_dir=$MODEL_DIR --train_steps=1000000 &
 echo "kill $!" > stop.sh
 # On worker0.example.com:
 for idx in $WORKERS; do
@@ -22,7 +22,7 @@ python3 cube.py \
      --ps_hosts=localhost:2222 \
      --worker_hosts=$WORKER_HOSTS \
      --job_name=worker --task_index=$idx \
-     --rolls=64 --rolls_len=32 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 &
+     --rolls=128 --rolls_len=64 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 --train_steps_per_eval=200 &
 echo "kill $!" >> stop.sh
 done
 chmod u+x stop.sh
