713361747e0b53137537c81a5c5a73cce630d1e0
tomas <tomaskrajco@instance-1.c.tpu-test-001.internal>
2018-09-18 11:41:33 +0000

    run-mpi

--- deepxor-async/run-mpi.sh
+++ deepxor-async/run-mpi.sh
@@ -1,2 +1,6 @@
-MODEL_DIR=gs://tomaskrajco-tpu-test-001-bucket/deepxor-async-gd-9
-mpirun -np 8 python3 cube.py  --rolls=128 --rolls_len=64 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 --train_steps_per_eval=200
+MODEL_DIR=gs://tomaskrajco-tpu-test-001-bucket/deepxor-async-gd-10
+mpirun 	-np 2 \
+	-bind-to none -map-by slot \
+	-x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
+	-mca pml ob1 -mca btl ^openib \
+	python3 cube.py  --rolls=64 --rolls_len=32 --model_dir=$MODEL_DIR --train_steps=1000000 --learning_rate=0.01 --train_steps_per_eval=100 --batch_size=32
